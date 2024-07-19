45b2cdea101874079d201dbbf85653a137f8b57d
Viet Hua <huaviet999@gmail.com>
2024-07-19 17:35:57 +0700

    [Leetcode] [Implement] [88] Merge Sorted Array

---
+++ src/main/java/com/vproject/study/leetcode/easy/Solution.java
@@ -0,0 +1,42 @@
+package com.vproject.study.leetcode.easy;
+
+public class Solution {
+    /**
+     * ID: 88
+     * Title: Merge Sorted Array
+     * Difficulty: Easy
+     * Link: <a href="https://leetcode.com/problems/merge-sorted-array/description/">...</a>
+     */
+    private void mergeSortedArray(int[] nums1, int m, int[] nums2, int n) {
+        int[] firstArray = new int[m];
+
+        for (int firstIndex = 0; firstIndex < firstArray.length; firstIndex++) {
+            firstArray[firstIndex] = nums1[firstIndex];
+        }
+        int firstPointer = 0;
+        int secondPointer = 0;
+        int mergedPointer = 0;
+
+        while (firstPointer < firstArray.length && secondPointer < n) {
+            if (firstArray[firstPointer] < nums2[secondPointer]) {
+                nums1[mergedPointer] = firstArray[firstPointer];
+                firstPointer++;
+            } else {
+                nums1[mergedPointer] = nums2[secondPointer];
+                secondPointer++;
+            }
+            mergedPointer++;
+        }
+
+        while (firstPointer < firstArray.length) {
+            nums1[mergedPointer] = firstArray[firstPointer];
+            firstPointer++;
+            mergedPointer++;
+        }
+        while (secondPointer < n) {
+            nums1[mergedPointer] = nums2[secondPointer];
+            secondPointer++;
+            mergedPointer++;
+        }
+    }
+}
